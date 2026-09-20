cc42d1b96da5c6d663f6ff38588ce820400ce88d
Adrianys Saumeth <adrianys2005@gmail.com>
2026-09-20 13:40:32 -0500

    feat(ea1): estructura base del proyecto Spring Boot Flores del Valle

---
+++ src/main/java/com/floresdelvalle/FloresdelvalleApplication.java
@@ -0,0 +1,12 @@
+package com.floresdelvalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresdelvalleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloresdelvalleApplication.class, args);
+    }
+}
